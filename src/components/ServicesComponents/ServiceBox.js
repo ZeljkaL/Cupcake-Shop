5267684422b9720c120ecb641978da9669f0a5e1
ZeljkaL <66870550+ZeljkaL@users.noreply.github.com>
2020-12-20 16:56:15 +0100

    "commit"

--- src/components/ServicesComponents/ServiceBox.js
+++ src/components/ServicesComponents/ServiceBox.js
@@ -1,21 +1,50 @@
-import React,{useEffect} from "react";
-import wreath from './wreath.png';
-import AOS from 'aos';
+import React, { useEffect } from "react";
+import wreath from "./wreath.png";
+import AOS from "aos";
 import "aos/dist/aos.css";
 
-function ServiceBox({title,paragraph,icon}) {
+function ServiceBox({ title, paragraph, icon }) {
   useEffect(() => {
     AOS.init({
-      duration : 2000
+      duration: 2000,
     });
   }, []);
   return (
-<div  className="service-box">
-  <img data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000"  src={wreath} alt='wreath'/>
-  <div data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000"  className='icon'>{icon}</div>
-  <h3 data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >{title}</h3>
-  <p  data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000">{paragraph}</p>
-</div>
+    <div className="service-box">
+      <img
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+        src={wreath}
+        alt="wreath"
+      />
+      <div
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+        className="icon"
+      >
+        {icon}
+      </div>
+      <h3
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+      >
+        {title}
+      </h3>
+      <p
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+      >
+        {paragraph}
+      </p>
+    </div>
   );
 }
 
--- src/components/SpecialOfferComponents/SpecialBox.js
+++ src/components/SpecialOfferComponents/SpecialBox.js
@@ -1,24 +1,64 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 
-function SpecialBox({title,paragraph,icon,price}) {
+function SpecialBox({ title, paragraph, icon, price }) {
   useEffect(() => {
     AOS.init({
-      duration : 2000
+      duration: 2000,
     });
   }, []);
   return (
-   
-<div data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" className="special-box">
-  <div className='dashed'  data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000">
-  <div data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='icon'>{icon}</div>
-  <h3 data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000">{title}</h3>
-  <p data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000">{paragraph}</p>
-  <h4 data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000">{price}$</h4>
-  </div>
-</div>
+    <div
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-easing="ease-in-sine"
+      data-aos-duration="1000"
+      className="special-box"
+    >
+      <div
+        className="dashed"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+      >
+        <div
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+          className="icon"
+        >
+          {icon}
+        </div>
+        <h3
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          {title}
+        </h3>
+        <p
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          {paragraph}
+        </p>
+        <h4
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+        >
+          {price}$
+        </h4>
+      </div>
+    </div>
   );
 }
 
