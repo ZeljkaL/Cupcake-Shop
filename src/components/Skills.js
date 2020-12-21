2b7fa2139346fb696de4e0d7627273044ea9e9b6
ZeljkaL <66870550+ZeljkaL@users.noreply.github.com>
2020-12-21 17:59:31 +0100

    "commit"

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,47 +1,59 @@
-import React,{Component} from "react";
-import Title from './Title'
-import Progress from './SkillsComponents/Progress'
-import hr from '../photos/png/hr.png'
-import AOS from 'aos';
+import React, { Component } from "react";
+import Title from "./Title";
+import Progress from "./SkillsComponents/Progress";
+import hr from "../photos/png/hr.png";
+import AOS from "aos";
 import "aos/dist/aos.css";
-export default class Skills extends Component{
- /*
+export default class Skills extends Component {
   state = {
-    i:0,
+    i: 0,
+  };
+  handleBar = () =>
+    this.setState({
+      i: true,
+    });
+
+  componentDidMount() {
+    // or simply just AOS.init();
+    AOS.init({
+      // initialise with other settings
+      duration: 2000,
+    });
   }
-  handleBar= () => this.setState({ 
-    i: true, 
-    
-  });
-  */
-  
-    componentDidMount() {
-        // or simply just AOS.init();
-        AOS.init({
-          // initialise with other settings
-          duration : 2000
-        });
-     
-    
-     
-      }
-    
- render(){
-  return (
-    <div  className="skills">
-     
-        <Title title='Skills' sr={hr}/>
-       <div data-aos="fade-up" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='skills-holder'>
-           <div className='pr-box first'> <Progress  done="70"/><p>Returning customers</p></div>
-           <div className='pr-box '> <Progress  done="60"/><p>Winning contests</p></div>
-           <div className='pr-box t'> <Progress  done="85"/><p>Unique items</p></div>
-           <div className='pr-box '> <Progress  done="90"/><p>Using own products</p></div>
-      
-       </div>
-    </div>
-      );
-  
- }
+
+  render() {
+    return (
+      <div onPointerOver={this.handleBar} className="skills">
+        <Title title="Skills" sr={hr} />
+        <div
+          data-aos="fade-up"
+          data-aos-offset="200"
+          data-aos-easing="ease-in-sine"
+          data-aos-duration="1000"
+          className="skills-holder"
+        >
+          <div className="pr-box first">
+            {" "}
+            <Progress i={this.state.i} done="70" />
+            <p>Returning customers</p>
+          </div>
+          <div className="pr-box ">
+            {" "}
+            <Progress i={this.state.i} done="60" />
+            <p>Winning contests</p>
+          </div>
+          <div className="pr-box t">
+            {" "}
+            <Progress i={this.state.i} done="85" />
+            <p>Unique items</p>
+          </div>
+          <div className="pr-box ">
+            {" "}
+            <Progress i={this.state.i} done="90" />
+            <p>Using own products</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
--- src/components/SkillsComponents/Progress.js
+++ src/components/SkillsComponents/Progress.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from "react";
 
+const Progress = ({ done, i }) => {
+  const [style, setStyle] = React.useState({});
 
+  setTimeout(() => {
+    const newStyle = {
+      opacity: 1,
+      width: `${done}%`,
+      animation: "move 2s linear ",
+    };
 
+    setStyle(newStyle);
+  }, 500);
 
-
-
-const Progress = ({done}) => {
-	const [style, setStyle] = React.useState({});
-
-	setTimeout(() => {
-		const newStyle = {
-			opacity: 1,
-			width: `${done}%`
-		}
-		
-		setStyle(newStyle);
-	}, 500);
-	
-	return (
-		<div id='boxbar'  className= "progress">
-			<div  className="progress-done"  style={style}>
-				{done}%
-			</div>
-		</div>
-	)
-
-}
+  return (
+    <div id="boxbar" className="progress">
+      <div
+        className={i ? "progress-done" : "progress-done disabled"}
+        style={style}
+      >
+        <style>{`
+            @keyframes move {
+                 0% { width: 0px; }
+                 100% {width: ${done}%; }
+            }
+        `}</style>
+        {done}%
+      </div>
+    </div>
+  );
+};
 
 export default Progress;
